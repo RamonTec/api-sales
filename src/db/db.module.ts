ba6ac4a87820ee6f22705617de4fb083244ea8f5
elias estrabao <eestrabao46@gmail.com>
2025-03-20 09:34:25 -0400

    added schema categorie into db

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/users/schemas/users.schemas';
+import { Categorie, CategorieSchema } from 'src/categories/schemas/categories.schemas';
 
 @Module({
     imports:[ 
         MongooseModule.forRoot('mongodb+srv://ramontec:123456789_10@cluster0.slohguw.mongodb.net/gameDB'),
         MongooseModule.forFeature([{name: User.name, schema: UserSchema}]),
+        MongooseModule.forFeature([{name: Categorie.name, schema: CategorieSchema}]),
     ],
     exports:[MongooseModule]
 })
